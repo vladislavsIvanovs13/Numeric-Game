2f7ba16ff464fe583a34e3703dd4393c1762e1b4
221RDB362 <122871094+Krissx680@users.noreply.github.com>
2024-03-13 17:09:15 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,17 @@ class Main:
     def __init__(self, window):
         self.window = window
         self.window.title("Numeric Game")
+        rules_text = """Spēles Noteikumi - Spēles sākumā ir dota ģenerētā skaitļu virkne.
+        Spēlētāji izpilda gājienus pēc kārtas.
+        Katram spēlētājam ir 0 punktu spēles sākumā.
+        Gājiena laikā spēlētājs var - saskaitīt skaitļu pāri un summu ierakstīt saskaitīto skaitļu pāra vieta vietā
+        kā arī pieskaitīt savam punktu skaitam 1 punktu, VAI nodzēst to skaitli, kas ir palicis bez pāra
+        un atņemt vienu punktu no sava punktu skaita.
+        Spēle beidzas, kad skaitļu virknē paliek viens skaitlis.
+        UZVAR SPĒLĒTĀJS, KAM IR VAIRĀK PUNKTI."""
+
+        rules_label = Label(window, text=rules_text, font=('Times New Roman', 12), justify=LEFT)
+        rules_label.place(x=180, y=20)
         self.button = Button(window, text="Generate string", height=2, width=15,
                              command=self.display_string)
         self.button.place(x=180, y=510)
@@ -29,5 +40,6 @@ if __name__ == "__main__":
     window.maxsize(1200, 800)
     Main(window)
     window.mainloop()
+
 
     
