10d4573cbfa0eecea01a2568e6233ed3cb6883fb
vladislavsIvanovs13 <vladislavs.ivanovs_3@edu.rtu.lv>
2024-03-23 22:43:30 +0200

    Add heuristic function and adapt tree construction to algorithms

--- game_tree.py
+++ game_tree.py
@@ -1,9 +1,14 @@
 #Aizgūts no studiju kursa materiāliem:
 # https://estudijas.rtu.lv/mod/resource/view.php?id=4161716
+from node_evaluator import Evaluator
 from string_generator import MAX_NUMBER
 
 POINT = 1
 BIG_NUMBERS = {7, 8, 9, 10, 11, 12}
+MOVE_ONE = '1'
+MOVE_TWO = '2'
+PAIR_LENGTH = 2
+DEPTH = 5
  
 class Node:
     def __init__(self, id, string, p1, p2, level, heu=0):
@@ -26,77 +31,87 @@ class Tree:
         self.edges[start_id] = self.edges.get(start_id, []) + [end_id]
         
 def check_step(step_type, generated_nodes, current_node, ptr):
-        global j
-        id_new = 'A' + str(j)
-        string_new = current_node[1]
+    global j
+    id_new = 'A' + str(j)
+    string_new = current_node.string
+    
+    if (step_type == MOVE_TWO) and (len(string_new) % 2 == 1) \
+            and (len(string_new) > 1):
+        string_new = string_new[:-1]
         
-        if (step_type == '2') and (len(string_new) % 2 == 1) \
-                and (len(string_new) > 1):
-            string_new = string_new[:-1]
-            
-        elif (step_type == '1'):
-            sum = int(string_new[ptr:ptr+1]) + \
-                  int(string_new[ptr+1:ptr+2])
-            
-            if sum in BIG_NUMBERS:
-                sum -= MAX_NUMBER
-                
-            string_new = string_new[:ptr] + str(sum) \
-                        + string_new[ptr+2:]
-            
-        else: return
+    elif (step_type == MOVE_ONE):
+        sum = int(string_new[ptr:ptr+1]) + \
+                int(string_new[ptr+1:ptr+2])
         
-        j += 1
-        if (current_node[4] % 2 == 0):
-            p1_new = current_node[2]
-            if (step_type == '1'):
-                p2_new = current_node[3] + POINT
-            else:
-                p2_new = current_node[3] - POINT
+        if sum in BIG_NUMBERS:
+            sum -= MAX_NUMBER
+            
+        string_new = string_new[:ptr] + str(sum) \
+                    + string_new[ptr+2:]
+        
+    else: return
+    
+    j += 1
+    if (current_node.level % 2 == 0):
+        p1_new = current_node.p1
+        if (step_type == MOVE_ONE):
+            p2_new = current_node.p2 + POINT
         else:
-            p2_new = current_node[3]
-            if (step_type == '1'):
-                p1_new = current_node[2] + POINT
-            else:
-                p1_new = current_node[2] - POINT
-        
-        level_new = current_node[4] + 1
+            p2_new = current_node.p2 - POINT
+    else:
+        p2_new = current_node.p2
+        if (step_type == MOVE_ONE):
+            p1_new = current_node.p1 + POINT
+        else:
+            p1_new = current_node.p1 - POINT
+    
+    level_new = current_node.level + 1
+    
+    if (level_new == DEPTH):
+        if (step_type == MOVE_ONE):
+            factor = 1
+        else:
+            factor = 0
+        heu_new = Evaluator.evaluate_node(p1_new, p2_new, factor)
+        node_new = Node(id_new, string_new, p1_new, p2_new, level_new, heu_new)    
+    else:
         node_new = Node(id_new, string_new, p1_new, p2_new, level_new)
-        
-        checked = False
-        i = 0
-        while (not checked) and (i <= len(game_tree.nodes) - 1):
-            if (game_tree.nodes[i].string == node_new.string) \
-                and (game_tree.nodes[i].p1 == node_new.p1) \
-                and (game_tree.nodes[i].p2 == node_new.p2) \
-                and (game_tree.nodes[i].level == node_new.level):
-                checked = True
-            else:
-                i += 1
-        if not checked:
-            game_tree.add_node(node_new)
-            generated_nodes.append([id_new, string_new, p1_new, p2_new, level_new])
-            game_tree.add_edge(current_node[0], id_new)
+    
+    checked = False
+    i = 0
+    while (not checked) and (i <= len(game_tree.nodes) - 1):
+        if (game_tree.nodes[i].string == node_new.string) \
+            and (game_tree.nodes[i].p1 == node_new.p1) \
+            and (game_tree.nodes[i].p2 == node_new.p2) \
+            and (game_tree.nodes[i].level == node_new.level):
+            checked = True
         else:
-            j -= 1
-            game_tree.add_edge(current_node[0], game_tree.nodes[i].id)
-     
+            i += 1
+    if not checked:
+        game_tree.add_node(node_new)
+        generated_nodes.append(Node(id_new, string_new, p1_new, p2_new, level_new))
+        game_tree.add_edge(current_node.id, id_new)
+    else:
+        j -= 1
+        game_tree.add_edge(current_node.id, game_tree.nodes[i].id)
+    
 game_tree = Tree()
 generated_nodes = []
-game_tree.add_node(Node('A1', '1632416324', 0, 0, 1))
+game_tree.add_node(Node('A1', '16324', 0, 0, 1))
 # 16324 should be replaced with the generated string
 # from string_generator class
-generated_nodes.append(['A1', '1632416324', 0, 0, 1])
+generated_nodes.append(Node('A1', '16324', 0, 0, 1))
 
 j = 2
-while len(generated_nodes) > 0:
+while (len(generated_nodes) > 0) and (generated_nodes[0].level <= DEPTH):
     current_node = generated_nodes[0]
     ptr = 0
-    while ptr < len(current_node[1]) - 1:
-        check_step('1', generated_nodes, current_node, ptr)
-        ptr += 2
-    check_step('2', generated_nodes, current_node, ptr)
+    while ptr < len(current_node.string) - 1:
+        check_step(MOVE_ONE, generated_nodes, current_node, ptr)
+        ptr += PAIR_LENGTH
+    check_step(MOVE_TWO, generated_nodes, current_node, ptr)
     generated_nodes.pop(0)
+
 
 for x in game_tree.nodes:
     print(x.id,x.string,x.p1,x.p2,x.level,x.heu)
---
+++ node_evaluator.py
@@ -0,0 +1,4 @@
+class Evaluator():
+    def evaluate_node(p1, p2, factor2):
+        factor1 = p1 - p2
+        return factor1 + factor2
