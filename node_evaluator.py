4397285237f9ebdb6b9914d463f36fd81669785b
KsenijaVoronecka <ksenija.voronecka@edu.rtu.lv>
2024-03-31 00:29:46 +0200

    Minimax algorith

--- node_evaluator.py
+++ node_evaluator.py
@@ -3,5 +3,10 @@ MOVE_ONE = '1'
 class Evaluator():
     def evaluate_node(p1, p2, step_type):
         factor1 = p1 - p2
+
+        print()
+        print("p1 = ", p1, ", p2 = ", p2)
+        print()
+
         factor2 = 1 if (step_type == MOVE_ONE) else 0
         return factor1 + factor2
